bbb0bbf212414e5b1f898aedb40c889812f79615
Jinwoo Kim <131782814+Greenviee@users.noreply.github.com>
2023-11-30 16:52:51 +0900

    Update revwords.c

--- revwords.c
+++ revwords.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 bool issymbol(char c) {
-	if (c == '?' || c == '!' || c == '.' || c == ',' || c == ';' || c == ':' || c == '\'' || c == '\"' || c == '-' || c == '_')
-		return true;
-	return false;
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9'))
+		return false;
+	return true;
 }
 
 int symbolcnt(char* s) {
@@ -22,25 +23,37 @@ char* filtersym(char* str) {
 	int i, j = 0;
 	int len = strlen(str);
 	for (i = 0; i < len; i++) {
-		if (len-i-j < 0)
+		if (len-i-j-1 < 0)
 			break;
-		while (issymbol(str[len-i-j]))
+		while (issymbol(str[len-i-j-1])) {
 			j++;
-		s[i] = str[len-i-j];
+			if (len-i-j-1 < 0)
+				break;
+		}
+		if (len-i-j-1 < 0)
+			break;
+		s[i] = str[len-i-j-1];
 	}
+	s[i] = NULL;
 	char* ret = malloc(sizeof(char)*(101-symbolcnt(str)));
 	strcpy(ret, s);
 	return ret;
 }
 
 int main() {
-	char str[101][101];
+	char str[256];
+	char split[101][101];
 	char answer[101][101];
 	int i = 0;
-	while (fgets(str[i], 101, stdin) == str[i]) {
-		strcpy(answer[i], filtersym(str[i]));
-		i++;
-	}
+	fgets(str, 256, stdin);
+	char *ptr = strtok(str, " ");    
+
+    while (ptr != NULL)              
+    { 
+    	strcpy(answer[i], filtersym(ptr));
+    	i++;
+        ptr = strtok(NULL, " ");      
+    }
 	int j;
 	for (j = 0; j < i; j++)
 		printf("%s ", answer[j]);
